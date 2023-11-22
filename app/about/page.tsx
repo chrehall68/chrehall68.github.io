6a5bfa1efc9dfb772e316c0b89fe3a7f7df3f15c
chrehall68 <chrehall68@gmail.com>
2023-11-21 17:38:05 -0800

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return <div>
+        <h1>About Me</h1>
+        <p>This is a super detailed description of me. Wooh! I love details! Ideally there would
+            be several paragraphs, each with an accompanying image in a style something like below:
+        </p>
+        <p>Text Thing BREAK Thing Text BREAK Text Thing</p>
+    </div>
+}
