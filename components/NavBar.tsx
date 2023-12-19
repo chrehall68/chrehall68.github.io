cd767a7c6de1f0490bd1a658d9fd8c2ac5f7ea45
chrehall68 <chrehall68@gmail.com>
2023-12-19 15:21:10 -0800

    fix build error

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 
 export function NavBar() {
-    return <div className="flex flex-inline items-center text-center w-full h-[10%] bg-gray-800 border-b border-white shadow-md shadow-black/5">
-        <Link href="/" className="p-10">Eliot Hall's Website</Link>
+    return <div className="flex flex-inline items-center text-center w-full h-[10%] bg-gray-800 border-b border-white">
+        <Link href="/" className="p-10">Eliot Hall&apos;s Website</Link>
         <span className="border-white border h-full"></span>
         <Link href="/" className="p-10">Home</Link>
         <span className="border-white border h-full"></span>
