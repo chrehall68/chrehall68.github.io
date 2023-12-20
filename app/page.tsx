409058edc24953ba2bea79ee85c164eb4664b44e
chrehall68 <chrehall68@gmail.com>
2023-12-20 09:53:31 -0800

    mobile layout type animation

--- app/page.tsx
+++ app/page.tsx
@@ -62,12 +62,17 @@ function UnderlinedTypeAnimation(prefix: string, items: string[], lettersPerMin:
     return () => clearInterval(tick)
   }, [item, idx, increasing, waitTime, opaque, interval, items, waitMS, blinkMS, blink])
 
-  return <div className="flex overflow-hidden rounded-lg bg-cover bg-[50%] bg-no-repeat text-center h-[50%]"
+  return <div className="flex overflow-hidden bg-cover bg-[50%] bg-no-repeat h-[50%] w-full"
     style={{ backgroundImage: "url('/codebg.png')" }}>
-    <div className="flex h-full w-full bg-black overflow-hidden bg-fixed items-center justify-center" style={{ backgroundColor: "rgba(0, 0, 0, 0.6" }}>
-      <div className="flex flex-inline jusitfy-center h-min ">
-        <p className="text-5xl">{prefix} <u>{items[item].substring(0, idx).replace(" ", '\xa0')}</u></p>
-        <span className={opaque ? "border border-white" : ""}></span>
+    <div className="flex h-full w-full bg-black overflow-hidden bg-fixed place-items-center" style={{ backgroundColor: "rgba(0, 0, 0, 0.6" }}>
+      <div className="w-full h-min flex flex-wrap justify-center">
+        <div className="flex flex-inline justify-center w-full lg:w-max">
+          <p className="text-3xl lg:text-5xl">{prefix}</p>
+        </div>
+        <div className="flex flex-inline justify-center w-full lg:w-min">
+          <p className="text-3xl lg:text-5xl">&nbsp;<u>{items[item].substring(0, idx).replace(" ", '\xa0')}</u></p>
+          <span className={opaque ? "border border-white" : ""}></span>
+        </div>
       </div>
     </div>
   </div>
