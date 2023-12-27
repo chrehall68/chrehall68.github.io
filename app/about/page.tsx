7108abeacabb6d86035de14822eb9b76a26f7da8
chrehall68 <chrehall68@gmail.com>
2023-12-26 21:18:28 -0800

    get building

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,6 +5,7 @@ import { Timeline } from "@/components/Timeline"
 import { BeakerIcon, MusicalNoteIcon, TvIcon } from "@heroicons/react/16/solid"
 import Link from "next/link"
 import React from "react"
+import Image from "next/image"
 
 export default function About() {
     return <Content>
@@ -12,19 +13,20 @@ export default function About() {
             {/** Quick Intro */}
             <div className="flex flex-inline w-full pb-2 items-center">
                 <p className="w-2/3 prose xl:px-[16.6%]">
-                    Hi there! My name is Christopher Eliot Hall, but I go by Eliot most of the time. I'm a
+                    Hi there! My name is Christopher Eliot Hall, but I go by Eliot most of the time. I&apos;m a
                     full time student getting my Bachelors of Science in Computer Science at
-                    San Jose State University. I'm currently a Freshman, but I'm hoping to graduate
+                    San Jose State University. I&apos;m currently a Freshman, but I&apos;m hoping to graduate
                     by Fall 2026 (a semester early).
                 </p>
-                <img src="/headshot.png" className="w-1/3 h-min rounded-full" alt="headshot" />
+                <img src="/headshot.png" alt="headshot" className="w-1/3 h-min rounded-full" />
+
             </div>
 
             {/** Hobbies */}
             <div>
                 <p>I spend most of my time working on fascinating coding projects
                     (check out my <Link href="/projects">project page</Link>) or completing schoolwork (graduating early
-                    isn't easy). That doesn't mean I don't get breaks, though! When I do get a break,
+                    isn&apos;t easy). That doesn&apos;t mean I don&apos;t get breaks, though! When I do get a break,
                     I like to spend my time:
                 </p>
                 <div className="flex flex-wrap justify-start lg:justify-around space-around">
